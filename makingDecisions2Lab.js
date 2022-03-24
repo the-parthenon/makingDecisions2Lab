3639cf3e63685ff987793b9d5da26199e6dbee26
the-parthenon <ericinportland@gmail.com>
2022-03-24 14:21:23 -0700

    Solutions through Problem 10 Implemented.

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -53,12 +53,16 @@ let evensArr = []
 
 //Code Here
 
-for (let i = 0; i < nums.length; i++){
-  if (nums[i] % 2 === 0){
-    evensArr.push(nums[i])
-  }
-}
+// for (let i = 0; i < nums.length; i++){
+//   if (nums[i] % 2 === 0){
+//     evensArr.push(nums[i])
+//     nums.splice(i, 1)
+//     i--;
+//   }
+// }
 // console.log(evensArr)
+// console.log(nums)
+
 
 
 ////////// PROBLEM 5 //////////
@@ -124,18 +128,18 @@ let someNum = myFavoriteNumbers[4]
 
 //Code Here
 
-if(myFavoriteNumbers.length < 7){
-  console.log("There are not enough elements in this array.")
-}else{
-  someNum = myFavoriteNumbers[6]
-}
+// if(myFavoriteNumbers.length < 7){
+//   console.log("There are not enough elements in this array.")
+// }else{
+//   someNum = myFavoriteNumbers[6]
+// }
 // console.log(myFavoriteNumbers)
 // console.log(someNum)
 
 ////////// PROBLEM 8 //////////
 
 // Do not edit the code below.
-var listOfNumbers = [1,2,"3",4,5,6,7,8,"ten",10,11,"12"];
+var listOfNumbers = [1,2,"3",4,5,6,7,8,9,10,11,"12"];
 // Do not edit the code above.
 
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. 
@@ -143,11 +147,11 @@ var listOfNumbers = [1,2,"3",4,5,6,7,8,"ten",10,11,"12"];
 
 //Code Here
 
-// for (let i = 0; i < listOfNumbers.length; i++){
-//   if (listOfNumbers[i] % 3 === 0){
-//     console.log(`${listOfNumbers[i]} is divisble by 3.`)
-//   }
-// }
+for (let i = 0; i < listOfNumbers.length; i++){
+  if (listOfNumbers[i] % 3 === 0){
+    console.log(`${listOfNumbers[i]} is divisble by 3.`)
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -160,18 +164,19 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-for (let i = letters.length-1; i >= 0; i--){
-  console.log(letters[i])
-}
+// for (let i = letters.length-1; i >= 0; i--){
+//   console.log(letters[i])
+// }
 
 
 ////////// Advanced Problems //////////
 
 ////////// PROBLEM 10 //////////
-// Switch statements can be excellent alternatives to if blocks. Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
+/* Switch statements can be excellent alternatives to if blocks. Look up switch statements 
+(I recommend W3 Schools) and try to implement one for the following.*/
 
 // Do not edit the code below.
-let letterGrade = 'B'
+let letterGrade = 'F'
 // Do not edit the code above.
 
 /* Use a switch statement on 'letterGrade' and console.log the appropriate response.
@@ -186,11 +191,38 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
+// switch (letterGrade){
+//   case "A":
+//     console.log("The student is doing excellently.")
+//     break;
+//   case "B":
+//     console.log("The student is doing well.")
+//     break;
+//   case "C":
+//     console.log("The student is doing alright.")
+//     break;
+//   case "D":
+//     console.log("The student is not doing very well.")
+//     break;
+//   case "F":
+//     console.log("The student is failing.")
+//     break;
+//   default:
+//     console.log("Not an eligible grade.")
+// }
+
+
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+  Create a for loop that iterates from 1 to 100. 
+  In this for loop, using some conditional logic, 
+  if the number your for loop is currently on is divisible by 3, console.log 'Dev'.
+   If the number is divisible by 5, console.log 'mountain'. 
+   If the number is divisible by 5 & 3, console.log 'Devmountain'. 
+   If the number is not divisible by 5 or 3, console.log the number itself. 
+   Hint: Look up the modulo operator.
 
   Your output should look like:
   1
