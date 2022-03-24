fa7774c4ed765cd33f510712ef1c4a5f2ecb9b6f
the-parthenon <ericinportland@gmail.com>
2022-03-24 14:31:44 -0700

    Final commit

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -245,3 +245,28 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+// for(i=1; i<=100; i++){
+
+//     if(i % 3 === 0 && i % 5 === 0){
+
+//         console.log("Devmountain")
+
+//     }
+//     else if(i % 3 === 0){
+
+//         console.log("Dev")
+
+//     }
+//     else if(i % 5 === 0){
+
+//         console.log("mountain")
+
+//     }
+//     else{
+
+//         console.log(i)
+
+//     }
+
+// }
