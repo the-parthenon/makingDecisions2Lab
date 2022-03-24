c5a553832cacda00eb6f3ef16a02f6d80ebe62ac
the-parthenon <ericinportland@gmail.com>
2022-03-24 13:02:54 -0700

    Solutions to Problem 1 & 2 implemented

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -13,10 +13,14 @@ console.log(firstItem)
 
 ////////// PROBLEM 2 //////////
 /*
-  Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'. Console.log the array to verify you no longer have the last item.
+  Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'.
+   Console.log the array to verify you no longer have the last item.
 */
 
 //Code Here
+
+let lastItemRemoved = arr.pop()
+console.log(arr)
 
 ////////// PROBLEM 3 //////////
 
